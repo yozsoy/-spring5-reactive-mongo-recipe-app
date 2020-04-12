519c46321ab665cbd8be76e22e3f4defe9549f78
y00562359 <ylozsoy@gmail.com>
2020-04-12 17:59:54 +0300

    Dependencies fixed and new mongo db repository added.

---
+++ src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -0,0 +1,11 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by jt on 8/17/17.
+ */
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+}
